78516ce3d8972d010b2a63e76127e31352e82205
Leonooore <eelyannora13@gmail.com>
2020-07-21 01:24:32 +0300

    task2 added constraint layout

--- hw_2_layouts/src/main/java/com/gmail/elnora/fet/hw_2_layouts/MainActivity.java
+++ hw_2_layouts/src/main/java/com/gmail/elnora/fet/hw_2_layouts/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task1_linear_layout;
 import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task1_table_layout;
+import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task2_constraint_layout;
 import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task2_linear_and_frame_layouts;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(task2_linear_and_frame_layouts);
+        setContentView(task2_constraint_layout);
     }
 }
