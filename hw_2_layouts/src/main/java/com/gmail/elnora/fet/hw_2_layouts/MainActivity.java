01873fc0ad1b08cff3133205406e925e32115a08
Leonooore <eelyannora13@gmail.com>
2020-07-21 00:10:27 +0300

    task2 added linear+frame layouts

--- hw_2_layouts/src/main/java/com/gmail/elnora/fet/hw_2_layouts/MainActivity.java
+++ hw_2_layouts/src/main/java/com/gmail/elnora/fet/hw_2_layouts/MainActivity.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 
 import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task1_linear_layout;
 import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task1_table_layout;
+import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task2_linear_and_frame_layouts;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(task1_linear_layout);
+        setContentView(task2_linear_and_frame_layouts);
     }
 }
