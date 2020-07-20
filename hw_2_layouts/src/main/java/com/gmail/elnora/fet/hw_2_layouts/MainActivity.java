03affd9e3314a674b460ce4255d5d4186ae045db
Leonooore <eelyannora13@gmail.com>
2020-07-20 21:54:16 +0300

    task1 added linear layout

--- hw_2_layouts/src/main/java/com/gmail/elnora/fet/hw_2_layouts/MainActivity.java
+++ hw_2_layouts/src/main/java/com/gmail/elnora/fet/hw_2_layouts/MainActivity.java
@@ -1,14 +1,16 @@
 package com.gmail.elnora.fet.hw_2_layouts;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task1_linear_layout;
+import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task1_table_layout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.task1_table_layout);
+        setContentView(task1_linear_layout);
     }
 }
