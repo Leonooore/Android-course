ece0009c778eebd88b9fa5f46441d53c271c1685
Leonooore <eelyannora13@gmail.com>
2020-07-21 23:14:38 +0300

    task4 added relative layout

--- hw_2_layouts/src/main/java/com/gmail/elnora/fet/hw_2_layouts/MainActivity.java
+++ hw_2_layouts/src/main/java/com/gmail/elnora/fet/hw_2_layouts/MainActivity.java
@@ -13,12 +13,13 @@ import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task3_linear_layout;
 import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task3_relative_layout;
 import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task4_constraint_layout;
 import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task4_linear_layout;
+import static com.gmail.elnora.fet.hw_2_layouts.R.layout.task4_relative_layout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(task4_constraint_layout);
+        setContentView(task4_relative_layout);
     }
 }
