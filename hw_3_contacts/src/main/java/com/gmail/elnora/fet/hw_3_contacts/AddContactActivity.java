ada3f23267d63be331087b63efcc128487da06c3
Leonooore <eelyannora13@gmail.com>
2020-07-29 23:54:27 +0300

    added radioButton listeners and test listener for buttonAddContact

--- hw_3_contacts/src/main/java/com/gmail/elnora/fet/hw_3_contacts/AddContactActivity.java
+++ hw_3_contacts/src/main/java/com/gmail/elnora/fet/hw_3_contacts/AddContactActivity.java
@@ -1,27 +1,83 @@
 package com.gmail.elnora.fet.hw_3_contacts;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddContactActivity extends AppCompatActivity {
     ImageButton buttonBack;
+    ImageButton buttonAdd;
+    RadioButton radioButtonPhoneNumber;
+    RadioButton radioButtonEmail;
+    EditText editTextName;
+    EditText editTextData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
 
+        buttonBack = findViewById(R.id.buttonBack);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        radioButtonPhoneNumber = findViewById(R.id.radioButtonPhoneNumber);
+        radioButtonEmail = findViewById(R.id.radioButtonEmail);
+        editTextName = findViewById(R.id.editTextName);
+        editTextData= findViewById(R.id.editTextData);
+
+        setListeners();
+    }
+
+    private void setListeners() {
         setButtonBackListener();
+        setButtonAddListener();
+        setRadioButtonPhoneNumberListener();
+        setRadioButtonEmailListener();
     }
 
     private void setButtonBackListener() {
-        buttonBack = findViewById(R.id.buttonBack);
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+    }
+
+    private void setButtonAddListener() {
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString();
+                String data = editTextData.getText().toString();
+                Intent result = new Intent(AddContactActivity.this, MainActivity.class);
+                result.putExtra("ADD_CONTACT", name);
+                setResult(RESULT_OK, result);
+                finish();
+            }
+        });
+    }
+
+    private void setRadioButtonPhoneNumberListener() {
+        radioButtonPhoneNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextData.setHint(R.string.contactPhoneNumber);
+                editTextData.setInputType(InputType.TYPE_CLASS_PHONE);
+            }
+        });
+    }
+
+    private void setRadioButtonEmailListener() {
+        radioButtonEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextData.setHint(R.string.contactEmail);
+                editTextData.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
             }
         });
     }
--- hw_3_contacts/src/main/java/com/gmail/elnora/fet/hw_3_contacts/MainActivity.java
+++ hw_3_contacts/src/main/java/com/gmail/elnora/fet/hw_3_contacts/MainActivity.java
@@ -1,8 +1,11 @@
 package com.gmail.elnora.fet.hw_3_contacts;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -23,9 +26,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddContactActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, 18000);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 18000 && resultCode == Activity.RESULT_OK && data != null) {
+            String name = data.getStringExtra("ADD_CONTACT");
+            Log.d("MainActivity", "Name: " + name);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
