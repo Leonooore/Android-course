48a38f3eca3f9abb305f3acde13b153f34aa55df
Leonooore <eelyannora13@gmail.com>
2020-08-05 21:30:57 +0300

    added changing sectors color

--- hw_4_customview/src/main/java/com/gmail/elnora/fet/hw_4_customview/CustomView.java
+++ hw_4_customview/src/main/java/com/gmail/elnora/fet/hw_4_customview/CustomView.java
@@ -5,11 +5,13 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class CustomView extends View {
+
     private Paint paintLeftTopSector = new Paint();
     private Paint paintRightTopSector = new Paint();
     private Paint paintLeftBottomSector = new Paint();
@@ -19,10 +21,10 @@ public class CustomView extends View {
     private int heightCenter;
     private int smallCircleRadius;
     private int bigCircleRadius;
-    private int left;
-    private int right;
-    private int top;
-    private int bottom;
+    private int leftSideBigCircle;
+    private int rightSideBigCircle;
+    private int topSideBigCircle;
+    private int bottomSideBigCircle;
 
     public CustomView(Context context) {
         super(context);
@@ -74,19 +76,19 @@ public class CustomView extends View {
         heightCenter = h / 2;
         smallCircleRadius = Math.min(w, h) / 8;
         bigCircleRadius = Math.min(w, h) / 3;
-        left = widthCenter - bigCircleRadius;
-        top = heightCenter - bigCircleRadius;
-        right = widthCenter + bigCircleRadius;
-        bottom = heightCenter + bigCircleRadius;
+        leftSideBigCircle = widthCenter - bigCircleRadius;
+        topSideBigCircle = heightCenter - bigCircleRadius;
+        rightSideBigCircle = widthCenter + bigCircleRadius;
+        bottomSideBigCircle = heightCenter + bigCircleRadius;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawArc(left, top, right, bottom, 0, 90, true, paintRightBottomSector);
-        canvas.drawArc(left, top, right, bottom, 90, 90, true, paintLeftBottomSector);
-        canvas.drawArc(left, top, right, bottom, 180, 90, true, paintLeftTopSector);
-        canvas.drawArc(left, top, right, bottom, 270, 90, true, paintRightTopSector);
+        canvas.drawArc(leftSideBigCircle, topSideBigCircle, rightSideBigCircle, bottomSideBigCircle, 0, 90, true, paintRightBottomSector);
+        canvas.drawArc(leftSideBigCircle, topSideBigCircle, rightSideBigCircle, bottomSideBigCircle, 90, 90, true, paintLeftBottomSector);
+        canvas.drawArc(leftSideBigCircle, topSideBigCircle, rightSideBigCircle, bottomSideBigCircle, 180, 90, true, paintLeftTopSector);
+        canvas.drawArc(leftSideBigCircle, topSideBigCircle, rightSideBigCircle, bottomSideBigCircle, 270, 90, true, paintRightTopSector);
         canvas.drawCircle(widthCenter, heightCenter, smallCircleRadius, paintSmallCircle);
     }
 
@@ -96,4 +98,40 @@ public class CustomView extends View {
         int b = (int)(Math.random()*256);
         return Color.rgb(r, g, b);
     }
+
+    private void changeAllSectorColor() {
+        paintRightBottomSector.setColor(getRandomColor());
+        paintLeftBottomSector.setColor(getRandomColor());
+        paintLeftTopSector.setColor(getRandomColor());
+        paintRightTopSector.setColor(getRandomColor());
+    }
+
+    private void changeColor(int eventX, int eventY) {
+        if ((eventX > widthCenter - smallCircleRadius && eventX < widthCenter + smallCircleRadius) &&
+                (eventY > heightCenter - smallCircleRadius && eventY < heightCenter + smallCircleRadius)) {
+                changeAllSectorColor();
+        } else if (eventX > leftSideBigCircle && eventX < widthCenter) { //if touched left side of circle
+            if (eventY > topSideBigCircle && eventY < heightCenter) { //left top side
+                paintLeftTopSector.setColor(getRandomColor());
+            } else if (eventY < bottomSideBigCircle && eventY > heightCenter) { //left bottom side
+                paintLeftBottomSector.setColor(getRandomColor());
+            }
+        } else if (eventX > widthCenter && eventX < rightSideBigCircle) { //if touched right side of circle
+            if (eventY > topSideBigCircle && eventY < heightCenter) { //right top side
+                paintRightTopSector.setColor(getRandomColor());
+            } else if ((eventY < bottomSideBigCircle && eventY > heightCenter)) { //right bottom side
+                paintRightBottomSector.setColor(getRandomColor());
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int eventX = (int) event.getX();
+        int eventY = (int) event.getY();
+        changeColor(eventX, eventY);
+        invalidate();
+        return super.onTouchEvent(event);
+    }
+
 }
--- hw_4_customview/src/main/java/com/gmail/elnora/fet/hw_4_customview/MainActivity.java
+++ hw_4_customview/src/main/java/com/gmail/elnora/fet/hw_4_customview/MainActivity.java
@@ -3,12 +3,17 @@ package com.gmail.elnora.fet.hw_4_customview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient.CustomViewCallback;
 
 public class MainActivity extends AppCompatActivity {
+    private CustomView customView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        customView = findViewById(R.id.customView);
+
     }
 }
