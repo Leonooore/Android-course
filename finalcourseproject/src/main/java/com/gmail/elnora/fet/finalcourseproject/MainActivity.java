5d04abb8156786eee13f35f91b45fd2941cd7ac5
Leonooore <eelyannora13@gmail.com>
2020-09-12 19:59:08 +0300

    added module for final course project: finalcourseproject

---
+++ finalcourseproject/src/main/java/com/gmail/elnora/fet/finalcourseproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gmail.elnora.fet.finalcourseproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
